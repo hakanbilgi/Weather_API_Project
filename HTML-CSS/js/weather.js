5ec92140d407b7ff840237051a186044bfe55d7f
Hakan Bilgi <hknblg34@gmail.com>
2022-09-28 21:18:24 +0300

    encrypt

---
+++ HTML-CSS/js/weather.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("section.top-banner form");
+const input = document.querySelector(".container input");
+const msg = document.querySelector("span.msg");
+const list = document.querySelector(".ajax-section .cities");
+// Api-Key 61842754ef2a8d32b10ea7bbebac1130
+localStorage.setItem(
+  "tokenKey",
+  "RAPAIooyOVFdRNn7gPi6i8vUp3OJvy0Np5wgMGgNO0a2a258kya95/arqJmhPrWc"
+);
+// localStorage.setItem(
+//   "tokenKeyEncrypted",
+//   EncryptStringAES("61842754ef2a8d32b10ea7bbebac1130")
+// );
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getWeatherDataFromApi();
+});
+
+//Get api func. (http methods == Verbs)
+const getWeatherDataFromApi = async () => {
+  //alert("http request is gone!");
+  const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
+  //alert(tokenKey);
+  const inputValue = input.value;
+  const units = "metric";
+  const lang = "tr";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
+
+  const response = await fetch(url).then((response) => response.json());
+  console.log(response);
+};
+
+
