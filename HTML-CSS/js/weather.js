db605841411f09377e617629754c3d1c864143fe
Hakan Bilgi <hknblg34@gmail.com>
2022-10-01 09:34:55 +0300

    src

--- HTML-CSS/js/weather.js
+++ HTML-CSS/js/weather.js
@@ -70,7 +70,11 @@ const getWeatherDataFromApi = async () => {
                             </figure>`;
     //append vs. prepend
     list.prepend(createdLi);
-
+createdLi.addEventListener("click", (e) => {
+  if (e.target.tagName == "IMG") {
+    e.target.src = e.target.src == iconUrl ? iconUrlAWS : iconUrl;
+  }
+});
     
   } catch (error) {
     console.log(error);
